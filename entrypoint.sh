c2e05f94bff899e3c809bd6c2ecc5e480749fab8
furkanulke-transkriptor <fulke@transkriptor.com>
2025-03-05 17:34:23 +0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# URL encode the AWS secret key for S3 URL (using sed instead of requiring gettext)
+AWS_SECRET_ACCESS_KEY_ENCODED=$(echo -n "$AWS_SECRET_ACCESS_KEY" | sed "s/\//%2F/g")
+
+# Replace variables in template using sed (no envsubst required)
+sed -e "s|\${AWS_ACCESS_KEY_ID}|$AWS_ACCESS_KEY_ID|g" \
+    -e "s|\${AWS_SECRET_ACCESS_KEY_ENCODED}|$AWS_SECRET_ACCESS_KEY_ENCODED|g" \
+    -e "s|\${AWS_REGION}|$AWS_REGION|g" \
+    -e "s|\${S3_BUCKET}|$S3_BUCKET|g" \
+    -e "s|\${LOKI_STORAGE_PATH}|$LOKI_STORAGE_PATH|g" \
+    /etc/loki/config.template.yml > /etc/loki/config.yml
+
+# Start Loki with the generated config
+exec /usr/bin/loki -config.file=/etc/loki/config.yml
